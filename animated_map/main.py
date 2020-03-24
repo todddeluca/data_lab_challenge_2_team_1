3d02225f62d050b5244a13ded60e262e03a2633c
Todd DeLuca <nospam@todddeluca.com>
2020-03-24 00:12:22 -0400

    Use notebook. Plot adjacency network

--- animated_map/main.py
+++ animated_map/main.py
@@ -1,6 +1,5 @@
 
 
-
 import argparse
 import geopandas
 import gzip
@@ -11,7 +10,6 @@ import plotly
 import plotly.express as px
 from pprint import pprint
 import requests
-
 
 
 def load_all_paper_data(download=False):
@@ -32,54 +30,83 @@ def load_all_paper_data(download=False):
     return trigger_na, trigger_ave, trigger_other, follow_up, follow_up_other, codebook
 
 
+def load_cases_confirmed():
+    return pd.read_csv('../data/ChiefdomCasesData-lab-confirmed-database.csv')
+
+
+def load_cases_suspected():
+    return pd.read_csv('../data/ChiefdomCasesData-suspected-database.csv')
+
+
 def load_digital_data(download=False):
     # content = gzip.decompress(requests.get("https://github.com/todddeluca/data_lab_challenge_2_team_1/raw/master/data/digita.csv.gz").content)
-    digital = pd.read_csv(
-        "https://github.com/todddeluca/data_lab_challenge_2_team_1/raw/master/data/digital.csv.gz",
-        low_memory=False)
+    if download:
+        url = "https://github.com/todddeluca/data_lab_challenge_2_team_1/raw/master/data/digital.csv.gz"
+    else:
+        url = '../data/digital.csv.gz'
+
+    digital = pd.read_csv(url, low_memory=False)
     # the digital codebook is not structured as a table.
     # See https://github.com/todddeluca/data_lab_challenge_2_team_1/raw/master/data/digital_codebook.xls
     return digital
 
 
 def load_chiefdom_adjacency_data(download=False):
-    url = 'https://raw.githubusercontent.com/todddeluca/data_lab_challenge_2_team_1/master/data/sierra_leone_chiefdom_adjacency.csv'
+    if download:
+        url = 'https://raw.githubusercontent.com/todddeluca/data_lab_challenge_2_team_1/master/data/sierra_leone_chiefdom_adjacency.csv'
+    else:
+        url = '../data/sierra_leone_chiefdom_adjacency.csv'
+
     adj = pd.read_csv(url, low_memory=False)
     return adj
 
 
 def load_chiefdom_geojson(download=False):
     '''return chiefdom geojson data as text'''
-    url = 'https://github.com/todddeluca/data_lab_challenge_2_team_1/raw/master/data/chiefdom.geojson'
-    return requests.get(url).json
+    if download:
+        url = 'https://github.com/todddeluca/data_lab_challenge_2_team_1/raw/master/data/chiefdom.geojson'
+        data = requests.get(url).json
+    else:
+        with open('../data/chiefdom.geojson') as fh:
+            data = json.load(fh)
+
+    return data
+
 
 def load_chiefdom_geopandas(download=False):
     '''return chiefdom geojson as geopandas'''
-    url = 'https://github.com/todddeluca/data_lab_challenge_2_team_1/raw/master/data/chiefdom.geojson'
-    chief = geopandas.read_file(url)
-    return chief
+    if download:
+        url = 'https://github.com/todddeluca/data_lab_challenge_2_team_1/raw/master/data/chiefdom.geojson'
+    else:
+        url = '../data/chiefdom.geojson'
 
-adj = download_chiefdom_adjacency_data()
-# adj
-trigger_na, trigger_ave, trigger_other, follow_up, follow_up_other, codebook = download_all_paper_data()
-# trigger_na, trigger_ave, trigger_other, follow_up, follow_up_other, codebook
-digital = download_digital_data()
-# digital
+    return geopandas.read_file(url)
 
-chief = download_chiefdom_geopandas()
-# chief
-chief_geojson = download_chiefdom_geojson()
-# chief_geojson
-chiefdom_geojson_filename = "chiefdom.geojson"
-chief.to_file(chiefdom_geojson_filename, driver = "GeoJSON")
-with open(chiefdom_geojson_filename) as fh:
-    chief_geojson = json.load(fh)
 
-# with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-with pd.option_context('display.max_rows', 20, 'display.max_columns', 10):  # more options can be specified also
-    print('num unique chiefdom:', trigger_ave['Chiefdom'].unique().shape[0])
-    print('how many observations per chiefdom in trigger_ave table?')
-    pprint(trigger_ave['Chiefdom'].value_counts())
+def load_all(download=False):
+    adj = load_chiefdom_adjacency_data()
+    print('adj', adj)
+    trigger_na, trigger_ave, trigger_other, follow_up, follow_up_other, codebook = load_all_paper_data()
+    print('all_paper', trigger_na, trigger_ave, trigger_other, follow_up, follow_up_other, codebook)
+    digital = load_digital_data()
+    print('digital', digital)
+
+    chief = load_chiefdom_geopandas()
+    print('chief', chief)
+    chief_geojson = load_chiefdom_geojson()
+    print('chief_geojson', chief_geojson)
+    # chiefdom_geojson_filename = "chiefdom.geojson"
+    # chief.to_file(chiefdom_geojson_filename, driver = "GeoJSON")
+    # with open(chiefdom_geojson_filename) as fh:
+    #     chief_geojson = json.load(fh)
+    # print('chief_geojson', chief_geojson)
+
+
+    # with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+    with pd.option_context('display.max_rows', 20, 'display.max_columns', 10):  # more options can be specified also
+        print('num unique chiefdom:', trigger_ave['Chiefdom'].unique().shape[0])
+        print('how many observations per chiefdom in trigger_ave table?')
+        pprint(trigger_ave['Chiefdom'].value_counts())
 
 """## Plot Chiefdoms in Plotly
 
@@ -90,7 +117,7 @@ Plotly Choropleth maps
 https://plot.ly/python/choropleth-maps/
 """
 
-def animated_map(chief, chief_geojson):
+def animate_map(chief, chief_geojson):
 
     # If your shapefile has coordinates in UTM format,
     # and you want to transform them into WGS84 (Lat / Lon format), you can do this:
@@ -115,18 +142,18 @@ def animated_map(chief, chief_geojson):
     fig.show()
 
 
-
-
 def main():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('task', help='a task to run.')
+    parser.add_argument('--download', action='store_true', default=False, help='download data from github')
     args = parser.parse_args()
 
-    if args.task == 'animated_map':
-        chief = download_chiefdom_geopandas()
-        chief_geojson = download_chiefdom_geojson()
+    if args.task == 'animate_map':
+        chief = load_chiefdom_geopandas()
+        chief_geojson = load_chiefdom_geojson()
         globals()[args.task](chief, chief_geojson)
-
+    elif args.task == 'load_all':
+        load_all(args.download)
 
 
 if __name__ == '__main__':
